1aeb4baf4cc4b1dbaf70ac5c3bf0542b7a50b24c
qtypatooty <20006949@myrp.edu.sg>
2022-07-04 12:50:44 +0800

    initial commit

--- web/index.php
+++ web/index.php
@@ -1,13 +1,3 @@
-<!-- fix : <html> element does not have a [lang] attribute -->
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Hello World!</title>
-    </head>
-    <body>
-    Hello World!   
-    </body>
-    </html>
+<?php
+
+echo "Hello World!";
