12b99829af555d9639ae9e170d5311e0fba16638
qtypatooty <20006949@myrp.edu.sg>
2022-07-04 12:47:43 +0800

    initial commit

---
+++ web/test.php
@@ -0,0 +1,13 @@
+<!-- fix : <html> element does not have a [lang] attribute -->
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Hello World!</title>
+    </head>
+    <body>
+    Hello World!   
+    </body>
+    </html>
